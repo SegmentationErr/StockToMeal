43c51cf61d065563c013d2317e2393512575362b
Haoran Zhang <haoranzhang9278@gmail.com>
2021-10-19 17:05:53 +0800

    Add the search button & search result basic layout

--- app/src/main/java/comp5216/sydney/edu/au/stocktomeal/RecipeRecommdationActivity.java
+++ app/src/main/java/comp5216/sydney/edu/au/stocktomeal/RecipeRecommdationActivity.java
@@ -2,13 +2,35 @@ package comp5216.sydney.edu.au.stocktomeal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RecipeRecommdationActivity extends AppCompatActivity {
+
+    // Search method
+    private EditText searchField;
+    private Button searchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_recommdation);
+
+        // search attribute
+        searchField = (EditText) findViewById(R.id.search_field);
+        searchButton = (Button) findViewById(R.id.search_button);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RecipeRecommdationActivity.this,
+                        SearchResultActivity.class);
+                intent.putExtra("search", searchField.getText().toString());
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/comp5216/sydney/edu/au/stocktomeal/SearchResultActivity.java
@@ -0,0 +1,28 @@
+package comp5216.sydney.edu.au.stocktomeal;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SearchResultActivity extends AppCompatActivity {
+
+    private String keywords;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_result);
+        Intent intent = getIntent();
+
+        if (intent != null && intent.getStringExtra("search") != null) {
+            keywords = intent.getStringExtra("search");
+            System.out.println("Terminal Print Keywords: " + keywords);
+        } else {
+            System.out.println("Intent doesn't put any keywords in.");
+            startActivity(new Intent(SearchResultActivity.this,
+                    RecipeRecommdationActivity.class) );
+        }
+
+    }
+}
