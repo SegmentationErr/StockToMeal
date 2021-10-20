2dcbe06bc21e947ed34c6f5725f1babc5a9657ed
Haoran Zhang <haoranzhang9278@gmail.com>
2021-10-20 21:03:09 +0800

    Recipe details page & search function

--- app/src/main/java/comp5216/sydney/edu/au/stocktomeal/RecipeDetailActivity.java
+++ app/src/main/java/comp5216/sydney/edu/au/stocktomeal/RecipeDetailActivity.java
@@ -5,6 +5,10 @@ import static android.content.ContentValues.TAG;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,43 +18,81 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 public class RecipeDetailActivity extends AppCompatActivity {
 
+    // The name of the recipe. Please do not upload the repeated recipes
     private String recipeName;
+
+    // Design of the layout
+    private TextView recipe;
+    private TextView ingredients;
+    private TextView steps;
+    private TextView flavour;
+    private ImageView image;
+    private Button favourite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.search_result);
+        setContentView(R.layout.recipe_detail);
         Intent intent = getIntent();
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-        if (intent != null && intent.getStringExtra("search") != null) {
-            recipeName = intent.getStringExtra("search");
-            System.out.println("Terminal Print Keywords: " + recipeName);
+        if (intent != null && intent.getStringExtra("recipe") != null) {
+            recipeName = intent.getStringExtra("recipe");
+            Log.d(TAG,"Terminal Print Keywords: " + recipeName);
         } else {
-            System.out.println("Intent doesn't put any keywords in.");
-            startActivity(new Intent(RecipeDetailActivity.this,
-                    RecipeRecommdationActivity.class) );
+            Log.d(TAG, "Intent doesn't put any keywords in.");
+            Intent other = new Intent(RecipeDetailActivity.this,
+                    RecipeRecommdationActivity.class);
+            other.putExtra("search", intent.getStringExtra("search"));
+            startActivity(other);
         }
 
-        DocumentReference docRef = db.collection("recipe").document(recipeName);
-        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful()) {
-                    DocumentSnapshot document = task.getResult();
-                    if (document.exists()) {
-                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
-                    } else {
-                        Log.d(TAG, "No such document");
-                    }
-                } else {
-                    Log.d(TAG, "get failed with ", task.getException());
-                }
-            }
-        });
+        // Init the layout
+        recipe = (TextView) findViewById(R.id.recipe_text);
+        ingredients = (TextView) findViewById(R.id.ingredients_text);
+        steps = (TextView) findViewById(R.id.step_text);
+        flavour = (TextView) findViewById(R.id.flavour_text);
+        image = (ImageView) findViewById(R.id.recipe_image);
+        favourite = (Button) findViewById(R.id.like_it_button);
 
+        // Get the recipe details from the database
+        db.collection("recipe")
+                .whereEqualTo("name", recipeName)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                recipe.setText((String) document.get("name"));
+                                String f = "";
+                                for (String fla : (ArrayList<String>) document.get("ingredients")) {
+                                    f += fla + "\n";
+                                }
+                                ingredients.setText(f);
+                                f = "";
+                                for (String fla : (ArrayList<String>) document.get("flavour")) {
+                                    f += fla + "\n";
+                                }
+                                flavour.setText(f);
+                                f = "";
+                                for (String fla : (ArrayList<String>) document.get("step")) {
+                                    f += fla + "\n";
+                                }
+                                steps.setText(f);
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
     }
 }
--- app/src/main/java/comp5216/sydney/edu/au/stocktomeal/SearchResultActivity.java
+++ app/src/main/java/comp5216/sydney/edu/au/stocktomeal/SearchResultActivity.java
@@ -1,44 +1,94 @@
 package comp5216.sydney.edu.au.stocktomeal;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 public class SearchResultActivity extends AppCompatActivity {
 
     private String keywords;
 
+    // Layout components
+    private ListView resultsView;
+    private ArrayAdapter<String> itemsAdapter;
+    private ArrayList<String> recipes;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_result);
+
+        // Property init
+        recipes = new ArrayList<>();
+        resultsView = (ListView) findViewById(R.id.search_container);
+        itemsAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, recipes);
         Intent intent = getIntent();
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
+        // Search keywords receive
         if (intent != null && intent.getStringExtra("search") != null) {
             keywords = intent.getStringExtra("search");
-            System.out.println("Terminal Print Keywords: " + keywords);
+            Log.d(TAG, "Terminal print keywords " + keywords);
         } else {
-            System.out.println("Intent doesn't put any keywords in.");
+            Log.d(TAG, "Intent doesn't put any keywords in.");
             startActivity(new Intent(SearchResultActivity.this,
                     RecipeRecommdationActivity.class) );
         }
 
-
-        /** Use ingredients to filter the search result
-        db.collection("recipe").whereEqualTo("name", keywords).get()
-                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+        // Use ingredients to filter the search result
+        db.collection("recipe").whereArrayContains("ingredients", keywords).get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
-                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                        // ...
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, "-" + document.getData());
+                                recipes.add((String) document.get("name"));
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                        resultsView.setAdapter(itemsAdapter);
                     }
-                });*/
+                });
 
+        setupListViewListener();
+    }
 
+    public void setupListViewListener() {
+        resultsView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String name = (String) itemsAdapter.getItem(position);
+                Log.d(TAG, "This is the recipe name: " + name);
+                Intent intent = new Intent(SearchResultActivity.this,
+                        RecipeDetailActivity.class);
+                if (intent != null) {
+                    // put "extras" into the bundle for access in the edit activity
+                    intent.putExtra("recipe", name);
+                    intent.putExtra("search", keywords);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
