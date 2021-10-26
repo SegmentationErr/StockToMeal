db929213160829ca20970ba9a07294c7154b1b56
SasakiBeryl <450988664@qq.com>
2021-10-26 19:09:49 +1100

    Update StockListActivity.java

--- app/src/main/java/comp5216/sydney/edu/au/stocktomeal/StockListActivity.java
+++ app/src/main/java/comp5216/sydney/edu/au/stocktomeal/StockListActivity.java
@@ -1,16 +1,21 @@
 package comp5216.sydney.edu.au.stocktomeal;
 
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterViewFlipper;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AlertDialog;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -49,12 +54,15 @@ public class StockListActivity extends AppCompatActivity {
         setContentView(R.layout.stock_list);
         firestoreList = findViewById(R.id.RecyclerList);
 
+        //connect firestore
         db = FirebaseFirestore.getInstance();
+        //connect auth, get current userID
         user = FirebaseAuth.getInstance().getCurrentUser();
         userID = user.getEmail();
 
-
+        //query current user stock status
         Query query = db.collection("food").whereEqualTo("userID",userID);
+
        FirestoreRecyclerOptions<Food> options = new FirestoreRecyclerOptions.Builder<Food>()
                 .setQuery(query,Food.class)
                 .build();
@@ -78,78 +86,9 @@ public class StockListActivity extends AppCompatActivity {
         firestoreList.setLayoutManager(new LinearLayoutManager(this));
         firestoreList.setAdapter(adapter);
 
-
-
-
-
-
-
-
-        /*
-        // Reference the "listView" variable to the id "lstView" in the layout
-        listView = (ListView) findViewById(R.id.lstView);
-
-        // Create an ArrayList of String
-        items = new ArrayList<String>(); items.add("Food 1"); items.add("Food 2");
-
-        // Create an adapter for the list view using Android's built-in item layout
-        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
-
-        // Connect the listView and the adapter
-        listView.setAdapter(itemsAdapter);
-
-        // Setup listView listeners
-        setupListViewListener();*/
     }
 
 
-
-    /*
-    private void setupListViewListener() {
-        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long rowId)
-            {
-                Log.i("StockListActivity", "Long Clicked item " + position);
-                AlertDialog.Builder builder = new AlertDialog.Builder(StockListActivity.this);
-                builder.setTitle(R.string.dialog_delete_title)
-                        .setMessage(R.string.dialog_delete_msg)
-                        .setPositiveButton(R.string.delete, new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                items.remove(position); // Remove item from the ArrayList
-                                itemsAdapter.notifyDataSetChanged(); // Notify listView adapter to update the list
-                            }
-                        })
-                        .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                // User cancelled the dialog
-                                // Nothing happens
-                            }
-                        });
-
-                builder.create().show();
-                return true;
-            }
-        });
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String stock = (String) itemsAdapter.getItem(position);
-                Log.i("StockListActivity", "Clicked item " + position + ": " + stock);
-
-                Intent intent = new Intent(StockListActivity.this, StockDetailActivity.class);
-                if (intent != null) {
-                    // put "extras" into the bundle for access in the detail activity
-                    intent.putExtra("stock", stock);
-                    intent.putExtra("position", position);
-
-                    startActivity(intent);
-                    itemsAdapter.notifyDataSetChanged();
-                }
-            }
-        });
-    }
-    */
 
 
     public void onAddButtonClick(View v) {
@@ -183,7 +122,7 @@ public class StockListActivity extends AppCompatActivity {
                             "Added: " + foodName,
                             Toast.LENGTH_SHORT).show();
 
-                    // TODO 更新对应数据！！！！！！！！！
+                    // Add new data to database
                     Food food = new Food(userID,foodName,amount,expireDate);
                     db.collection("food").document(foodName + "_" + userID).set(food);
 
